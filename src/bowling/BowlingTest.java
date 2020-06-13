c582b984fa74ea554475d87c34d43051fe021e2b
sunlike0508 <thesin1989@gmail.com>
2020-06-13 16:15:49 +0900

    리펙토링

--- src/bowling/BowlingTest.java
+++ src/bowling/BowlingTest.java
@@ -1,22 +1,23 @@
 package bowling;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BowlingTest {
-
-	@Test
+	
+	Bowling bowling;
+	
+	@BeforeEach
 	@DisplayName("클래스 생성 가능")
-	void test_can_make_game_class() {
-		Bowling bowling = new Bowling();
+	void setUp() {
+		bowling = new Bowling();
 	}
 	
 	@Test
 	@DisplayName("roll 메소드 생성 가능")
 	void test_can_make_roll_function() {
-		Bowling bowling = new Bowling();
+		bowling = new Bowling();
 		bowling.roll(0);
 	}
 
